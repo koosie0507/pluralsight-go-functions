70397c23d4decf9c390b1e8fab49f4170c10c7de
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-26 16:01:52 +0300

    Improve the code structure

--- src/cmd/app/main.go
+++ src/cmd/app/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"./semver"
-	"./simplemath"
 	"fmt"
+	"semver"
+	"simplemath"
 )
 
 func main() {
-	ans, _ := simplemath.Divide (1, 0)
+	ans, _ := simplemath.Divide(1, 0)
 	fmt.Printf("%f\n", ans)
 	numbers := []float64{1, 3.14, 2, 6.22, 4.21}
 	fmt.Printf("%f\n", simplemath.Sum(numbers...))
