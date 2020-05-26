2feb59bfda64dc193f496f193172edcc1eb6dfa4
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-26 15:27:32 +0300

    Add more functions. Use a write-only var

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
+func main() {
+	ans, _ := divide (1, 0)
+	fmt.Printf("%f", ans)
+}
+
+func add(p1, p2 float64) float64 {
+	return p1 + p2
+}
+
+func subtract(p1, p2 float64) float64 {
+	return p1 - p2
+}
+
+func multiply(p1, p2 float64) float64  {
+	return p1 * p2
+}
+
+func divide(p1, p2 float64) (float64, error) {
+	if p2 == 0 {
+		return math.NaN(), errors.New("cannot divide by zero")
+	}
+	return p1 / p2, nil
 }
